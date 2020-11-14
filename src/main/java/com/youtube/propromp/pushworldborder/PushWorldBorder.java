14c05e068db3eebca4add7d173eba69f1d61a07d
Propromp <proprompx2@gmail.com>
2020-11-14 19:32:24 +0900

    command

--- src/main/java/com/youtube/propromp/pushworldborder/PushWorldBorder.java
+++ src/main/java/com/youtube/propromp/pushworldborder/PushWorldBorder.java
@@ -1,10 +1,12 @@
 package com.youtube.propromp.pushworldborder;
 
+import com.youtube.propromp.pushworldborder.commands.PWBCommand;
 import com.youtube.propromp.pushworldborder.events.PWBPlayerMoveEvent;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PushWorldBorder extends JavaPlugin {
-
+    public static FileConfiguration config;
     @Override
     public void onEnable() {
         this.getLogger().info("Hi!");
@@ -13,9 +15,16 @@ public final class PushWorldBorder extends JavaPlugin {
         new PWBPlayerMoveEvent(this);
 
         //コマンド登録
-//        getCommand("pwb").setExecutor();
-    }
+        try {
+            this.getCommand("pwb").setExecutor(new PWBCommand());
+        } catch(NullPointerException e){
+            e.printStackTrace();
+        }
 
+        //コンフィグ読み込み
+        saveDefaultConfig();
+        config = getConfig();
+    }
     @Override
     public void onDisable() {
         // Plugin shutdown logic
--- src/main/java/com/youtube/propromp/pushworldborder/events/PWBPlayerMoveEvent.java
+++ src/main/java/com/youtube/propromp/pushworldborder/events/PWBPlayerMoveEvent.java
@@ -1,10 +1,7 @@
 package com.youtube.propromp.pushworldborder.events;
 
-import net.minecraft.server.v1_15_R1.World;
+import com.youtube.propromp.pushworldborder.PushWorldBorder;
 import org.bukkit.WorldBorder;
-import org.bukkit.craftbukkit.v1_15_R1.CraftWorld;
-import org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer;
-import net.minecraft.server.v1_15_R1.EntityPlayer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -20,7 +17,7 @@ public class PWBPlayerMoveEvent implements Listener {
     public void PlayerMove(PlayerMoveEvent e){
         WorldBorder worldborder = e.getPlayer().getWorld().getWorldBorder();
         if(e.getPlayer().getLocation().distance(worldborder.getCenter()) > worldborder.getSize()/2 + 1.0) {
-            e.getPlayer().getWorld().getWorldBorder().setSize(e.getPlayer().getWorld().getWorldBorder().getSize() + 0.1);
+            e.getPlayer().getWorld().getWorldBorder().setSize(e.getPlayer().getWorld().getWorldBorder().getSize() + PushWorldBorder.config.getDouble("speed"));
         }
     }
 }
